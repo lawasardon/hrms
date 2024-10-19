6ea1c3c9bb078b08f9f988ccff7669828b0719d4
Cymer Denampo <cymerdenampo@gmail.com>
2024-10-19 16:11:52 +0800

    update

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="page-sub-header">
-                    <h3 class="page-title">Welcome {{Auth::user()->name}}!</h3>
+                    <h3 class="page-title">Welcome {{ Auth::user()->name }}!</h3>
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
                         <li class="breadcrumb-item active">Dashboard</li>
@@ -14,39 +14,39 @@
         </div>
     </div>
 
-
-    <div class="row">
-        <div class="col-xl-3 col-sm-6 col-12 d-flex">
-            <div class="card bg-comman w-100">
-                <div class="card-body">
-                    <div class="db-widgets d-flex justify-content-between align-items-center">
-                        <div class="db-info">
-                            <h6>Aqua</h6>
-                            <h3>50055</h3>
-                        </div>
-                        <div class="db-icon">
-                            <img src="{{ asset('admin/assets/img/icons/dash-icon-01.svg') }}" alt="Dashboard Icon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-sm-6 col-12 d-flex">
-            <div class="card bg-comman w-100">
-                <div class="card-body">
-                    <div class="db-widgets d-flex justify-content-between align-items-center">
-                        <div class="db-info">
-                            <h6>Laminin</h6>
-                            <h3>50055</h3>
-                        </div>
-                        <div class="db-icon">
-                            <img src="{{ asset('admin/assets/img/icons/dash-icon-01.svg') }}" alt="Dashboard Icon">
+    @hasanyrole('admin|hr')
+        <div class="row">
+            <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                <div class="card bg-comman w-100">
+                    <div class="card-body">
+                        <div class="db-widgets d-flex justify-content-between align-items-center">
+                            <div class="db-info">
+                                <h6>Aqua</h6>
+                                <h3>50055</h3>
+                            </div>
+                            <div class="db-icon">
+                                <img src="{{ asset('admin/assets/img/icons/dash-icon-01.svg') }}" alt="Dashboard Icon">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        {{-- <div class="col-xl-3 col-sm-6 col-12 d-flex">
+            <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                <div class="card bg-comman w-100">
+                    <div class="card-body">
+                        <div class="db-widgets d-flex justify-content-between align-items-center">
+                            <div class="db-info">
+                                <h6>Laminin</h6>
+                                <h3>50055</h3>
+                            </div>
+                            <div class="db-icon">
+                                <img src="{{ asset('admin/assets/img/icons/dash-icon-01.svg') }}" alt="Dashboard Icon">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- <div class="col-xl-3 col-sm-6 col-12 d-flex">
             <div class="card bg-comman w-100">
                 <div class="card-body">
                     <div class="db-widgets d-flex justify-content-between align-items-center">
@@ -61,37 +61,38 @@
                 </div>
             </div>
         </div> --}}
-        <div class="col-xl-3 col-sm-6 col-12 d-flex">
-            <div class="card bg-comman w-100">
-                <div class="card-body">
-                    <div class="db-widgets d-flex justify-content-between align-items-center">
-                        <div class="db-info">
-                            <h6>Department</h6>
-                            <h3>30+</h3>
+            <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                <div class="card bg-comman w-100">
+                    <div class="card-body">
+                        <div class="db-widgets d-flex justify-content-between align-items-center">
+                            <div class="db-info">
+                                <h6>Department</h6>
+                                <h3>30+</h3>
+                            </div>
+                            <div class="db-icon">
+                                <img src="{{ asset('admin/assets/img/icons/dash-icon-03.svg') }}" alt="Dashboard Icon">
+                            </div>
                         </div>
-                        <div class="db-icon">
-                            <img src="{{ asset('admin/assets/img/icons/dash-icon-03.svg') }}" alt="Dashboard Icon">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                <div class="card bg-comman w-100">
+                    <div class="card-body">
+                        <div class="db-widgets d-flex justify-content-between align-items-center">
+                            <div class="db-info">
+                                <h6>Revenue</h6>
+                                <h3>$505</h3>
+                            </div>
+                            <div class="db-icon">
+                                <img src="{{ asset('admin/assets/img/icons/dash-icon-04.svg') }}" alt="Dashboard Icon">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-sm-6 col-12 d-flex">
-            <div class="card bg-comman w-100">
-                <div class="card-body">
-                    <div class="db-widgets d-flex justify-content-between align-items-center">
-                        <div class="db-info">
-                            <h6>Revenue</h6>
-                            <h3>$505</h3>
-                        </div>
-                        <div class="db-icon">
-                            <img src="{{ asset('admin/assets/img/icons/dash-icon-04.svg') }}" alt="Dashboard Icon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @endhasrole
 
     {{-- <div class="row">
         <div class="col-md-12 col-lg-6">
